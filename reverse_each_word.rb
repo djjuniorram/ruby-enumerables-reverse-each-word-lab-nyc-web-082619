a4406a87640d07bfe7bf940f8d76d0e201e13044
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-08-23 17:26:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 def reverse_each_word(string)
   string.split("")
+
+
+def reverse_each_word_with_each(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do|string|
+    new_array << string.reverse
+  end
+  return_array.join(" ")
 end
